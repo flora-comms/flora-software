d00aebeef5c104abefb737471cc9f8d931e8bf27
Cameron Gillingham <144290859+camerongillingham93@users.noreply.github.com>
2024-09-18 09:14:02 -0700

    Adjusted Message bubble colours

--- web-app/chat-ui/obj/Debug/net8.0/chat-ui.AssemblyInfo.cs
+++ web-app/chat-ui/obj/Debug/net8.0/chat-ui.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chat-ui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b594364ed8090cf87b1d8d590697f2af919700d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7f08322e767d56de079460ad896393b45259ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("chat-ui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chat-ui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
