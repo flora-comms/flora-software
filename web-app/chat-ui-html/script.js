5e4cc42f23444f4dbf4194efafa99b9e0ea98ac6
Cameron Gillingham <144290859+camerongillingham93@users.noreply.github.com>
2024-10-08 13:12:58 -0700

    removed @ symbols from @onclick

--- web-app/chat-ui-html/script.js
+++ web-app/chat-ui-html/script.js
@@ -5,10 +5,11 @@ function sendMessage()
     let inputElement = document.getElementById("message");
     let messageText = inputElement.value;
     // add to array if conditions are met 
-    if(messageText.Trim() !== "")
+    if(messageText.trim() !== "")
     {
         messages.push(messageText)
         inputElement.value = "";
         console.log(messages);
     }
 }
+
