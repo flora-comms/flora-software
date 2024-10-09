f3d5fe83ef0e4453e50d09d21679293c090e0174
Cameron Gillingham <144290859+camerongillingham93@users.noreply.github.com>
2024-10-09 13:00:53 -0700

    comments

--- web-app/chat-ui/obj/Debug/net8.0/chat-ui.AssemblyInfo.cs
+++ web-app/chat-ui/obj/Debug/net8.0/chat-ui.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chat-ui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72bcd69d7b34159600088c957d11d6ec08aa45a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19df30ec7bc5fb1e2e9b74230b76844b324bb92")]
 [assembly: System.Reflection.AssemblyProductAttribute("chat-ui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chat-ui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
