881b0a87a90ef62ae219b20b8a30d9e92eaac251
Cameron Gillingham <144290859+camerongillingham93@users.noreply.github.com>
2024-10-15 08:32:53 -0700

    changed URL for pull request

--- web-app/chat-ui-html/script.js
+++ web-app/chat-ui-html/script.js
@@ -81,7 +81,7 @@ let socket;
 
 function openWebSocket() {
   socket =
-      new WebSocket('ws://192.168.1.69:8080');  // change to DNS URL once setup
+      new WebSocket('ws://avalink.local');  // change to DNS URL once setup
   socket.onopen = function(event) {
     console.log('WebSocket is connected.');
     publicChatHandler.parseHistory('history.JSON');
