7c7ec36ccaf4d1550d0db9ec0312fd803ec23f4c
aahuinink <a_huinink@hotmail.ca>
2024-10-17 08:42:35 -0700

    basic websocket working on with lilygo t3 s3

--- firmware/AVAlink-beta/src/main.cpp
+++ firmware/AVAlink-beta/src/main.cpp
@@ -16,19 +16,62 @@ Firmware for AVAlink nodes
 #include <SD.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
+#include <ESPmDNS.h>
 
 // ---------------- DEFINES ---------------- //
 
-// LILYGO T3 S3 V1.2
-#define SD_CS   13    // SD chip select pin
-#define SD_SCK  14    // SD sck pin
-#define SD_MOSI 11    // SD mosi pin
-#define SD_MISO 2     // SD miso
+/*
+ENVIRONMENT
 
+Uncomment define for the IDE uses. MAKE SURE ONLY ONE IS UNCOMMENTED
+
+*/
+#define ENV_PLATFORMIO
+// #define ENV_ARDUINO
+
+/*
+FIRMWARE
+
+Variables for firmware configuration
+*/
+#define WIFI_SSID "AVAlink"             // WiFi network name
+#define WEBSERVER_DNS       "avalink"   // puts domain at "https://{WEBSERVER_DNS}.local"
+#define WEBSOCKET_ENDPOINT  "/chat"     // puts websocket at "ws://{WEBSERVER_DNS}/{WEBSOCKET_ENDPOINT}"
+
+/*
+DEBUGGING
+
+Comment out for debugging
+*/
+#define DEBUG
+
+
+#ifdef DEBUG
+  #define SERIAL_BAUD       115200
+  #define DBG_PRINTLN(MSG)  Serial.println(MSG)
+#else
+  #define DBG_PRINTLN(MSG) 
+#endif
+
+/*
+HARDWARE
+
+Options for Hardware
+*/
+
+#define LILYGO_T3_S3_V1_2
+
+// LILYGO T3 S3 V1.2
+#ifdef LILYGO_T3_S3_V1_2
+  #define SD_CS   13    // SD chip select pin
+  #define SD_SCK  14    // SD sck pin
+  #define SD_MOSI 11    // SD mosi pin
+  #define SD_MISO 2     // SD miso
+#endif
 // ---------------- GLOBALS ---------------- //
 
 AsyncWebServer server(80);    // web server on port 80
-AsyncWebSocket ws("/chat");   // web socket at "ws://avalink.local/chat"
+AsyncWebSocket ws(WEBSOCKET_ENDPOINT); 
 
 // ---------------- CLASSES ---------------- //
 
@@ -37,19 +80,85 @@ AsyncWebSocket ws("/chat");   // web socket at "ws://avalink.local/chat"
 // ---------------- PROTOTYPES ---------------- //
 
 /// @brief avalink.local/chat websocket event handler
-/// @param server 
-/// 
-/// @param client 
-/// @param type 
-/// @param arg 
-/// @param data 
-/// @param len 
+/// @param server The WebSocket server/// 
+/// @param client The webSocket client
+/// @param type   The type of WebSocket event
+/// @param arg    
+/// @param data   Data buffer included in the websocket frame
+/// @param len    Length of the data buffer
 void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
 
 // ---------------- MAIN ---------------- //
 
 void setup() {
-  
+  #ifdef DEBUG
+  Serial.begin(SERIAL_BAUD);
+  #endif
+
+  #ifdef ENV_PLATFORMIO
+  delay(5000); // to give time to restart serial monitor task
+  #endif
+
+  // Set up Wi-Fi (AP mode)
+  DBG_PRINTLN("Setting up Access Point with SSID: ");
+  if (WiFi.softAP(WIFI_SSID))
+  {
+    DBG_PRINTLN("Access Point setup complete");
+  }
+  else
+  {
+    DBG_PRINTLN("Failed to set up Access Point");
+  }
+
+  DBG_PRINTLN("Access Point IP address: ");
+  DBG_PRINTLN(WiFi.softAPIP());
+
+  // Initialize the SD card
+
+  #ifdef ENV_PLATFORMIO
+    DBG_PRINTLN("Initializing SD card...");
+    SPI.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
+    SPI.setDataMode(SPI_MODE0);
+  #endif
+
+  #ifdef LILYGO_T3_S3_V1_2
+    #ifdef ENV_ARDUINO
+      #define REASSIGN_PINS
+      int sck = SD_SCK;
+      int mosi = SD_MOSI;
+      int miso = SD_MISO;
+    #endif
+  #endif
+
+  if (!SD.begin(SD_CS))
+  {
+    DBG_PRINTLN("Failed to mount SD card!");
+    return;
+  }
+  else
+  {
+    DBG_PRINTLN("SD card mounted successfully.");
+  }
+
+  // Start server
+
+  server.begin();
+  DBG_PRINTLN("Web server started!");
+
+  server.serveStatic("/", SD, "/").setDefaultFile("/index.html");
+  server.addHandler(&ws);
+
+  // start DNS
+
+  if (!MDNS.begin(WEBSERVER_DNS)) {
+    DBG_PRINTLN("Error setting up MDNS responder!");
+  }
+
+  MDNS.addService("http", "tcp", 80);
+
+  // add websocket service
+  ws.onEvent(onWsEvent);
+
 }
 
 void loop() {
@@ -57,3 +166,31 @@ void loop() {
 }
 
 // ---------------- DEFINITIONS ---------------- //
+void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  if (type == WS_EVT_CONNECT)
+  {
+    DBG_PRINTLN("Websocket client connection received");
+  }
+  else if (type == WS_EVT_DISCONNECT)
+  {
+
+    DBG_PRINTLN("Client disconnected");
+  }
+  else if (type == WS_EVT_DATA)
+  {
+
+    ws.textAll(data, len);
+
+    #ifdef DEBUG
+      Serial.print("Data received: ");
+
+      for (int i = 0; i < len; i++)
+      {
+        Serial.print((char)data[i]);
+      }
+
+      Serial.println();
+    #endif
+  }
+}
+
