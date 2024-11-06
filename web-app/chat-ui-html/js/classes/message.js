b297f49f06535fd4727cea36b2eca47f3f7bcb63
Cameron Gillingham <144290859+camerongillingham93@users.noreply.github.com>
2024-11-06 07:23:39 -0800

    Revert "station change"
    This reverts commit 708470e9cefdff87e3b11f65bf9c54de5dcd1a65.

--- web-app/chat-ui-html/js/classes/message.js
+++
@@ -1,7 +0,0 @@
-export class Message {
-    constructor(content) {
-      this.Payload = content;
-      this.NodeID = myNodeID;  // myNodeID will be set after readLookup()
-      this.SOS = 0;
-    }
-  }
