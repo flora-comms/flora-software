f5328ae5163c45a68ff17893bcded7edbc7c8ea7
Cameron Gillingham <144290859+camerongillingham93@users.noreply.github.com>
2024-12-10 12:17:45 -0800

    dont need separate versions for mobile and desktop

--- web-app/chat-ui-mobile/src/classes/message.js
+++
@@ -1,7 +0,0 @@
-export default class Message {
-    constructor(content, nodeId) {
-        this.Payload = content;
-        this.NodeID = nodeId; 
-        this.SOS = 0;
-    }
-  }
--- web-app/chat-ui-mobile/src/utilities/modal.js
+++
@@ -1,9 +0,0 @@
-export function showModal() {
-    const modal = document.getElementById('sosModal');
-    modal.style.display = 'flex';
-  }
-  
-export function closeModal() {
-    const modal = document.getElementById('sosModal');
-    modal.style.display = 'none';
-  }
